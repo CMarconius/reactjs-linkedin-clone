724c345ed094b70114a79f59a419921fecad6e49
Marconius <marconius.dev@gmail.com>
2021-06-24 23:01:42 +0100

    Updated Article Component

--- src/components/Main.js
+++ src/components/Main.js
@@ -59,6 +59,43 @@ const Main = (props) => {
                         </a>
                     </SharedImg>
 
+                    <SocialCounts>
+                        <li>
+                            <button>
+                                <img src="/images/like-icon.svg" alt="" />
+                                <img src="/images/clapping-icon.svg" alt="" />
+
+                                <span>75</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>
+                                2 comments
+                            </a>
+                        </li>
+                    </SocialCounts>
+
+                    <SocialActions>
+                        <button>
+                            <img src="/images/like-icon-2.svg" alt="" />
+                            <span>Like</span>
+                        </button>
+
+                        <button>
+                            <img src="/images/comment-icon.svg" alt="" />
+                            <span>Comments</span>
+                        </button>
+
+                        <button>
+                            <img src="/images/share-icon.svg" alt="" />
+                            <span>Share</span>
+                        </button>
+
+                        <button>
+                            <img src="/images/send-icon.svg" alt="" />
+                            <span>Send</span>
+                        </button>
+                    </SocialActions>
                 </Article>
             </div>
 
@@ -242,6 +279,48 @@ const SharedImg = styled.div`
         width: 100%;
         height: 100%;
         
+    }
+`;
+
+const SocialCounts = styled.ul`
+    line-height: 1.3;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1px solid #e9e5df;
+    list-style: none;
+    li {
+        margin-right: 5px;
+        font-size: 12px;
+        button {
+            display: flex;
+        }
+    }
+`;
+
+const SocialActions = styled.div`
+    img {
+        filter: invert(24%) sepia(70%) saturate(2030%) hue-rotate(199deg) brightness(91%) contrast(83%);
+    }
+
+
+    align-items: center;
+    display: flex;
+    margin: 0;
+    min-height: 40px;
+    padding: 4px 8px;
+    button {
+        display: inline-flex;
+        align-items: center;
+        padding: 8px;
+        color: #0a66c2;
+        @media (min-width: 768px) {
+            span {
+                margin-left: 8px;
+            }
+        }
     }
 `;
 
